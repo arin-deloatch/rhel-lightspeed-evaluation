e900a459967d611c9942cbdbd21d10395e1a2aaf
arin-deloatch <arindeloatch@gmail.com>
2025-12-08 15:10:58 -0700

    housekeeping: toml & lock updates, reworked system file to include panel + generated evaluation data; QOL updates

--- src/rhel_lightspeed_evaluation/extensions/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Framework extensions for lightspeed-evaluation."""
-
-from rhel_lightspeed_evaluation.extensions.geval import GEvalMetrics
-from rhel_lightspeed_evaluation.extensions.panel_judges import PanelLLMManager
-
-__all__ = ["PanelLLMManager", "GEvalMetrics"]
--- src/rhel_lightspeed_evaluation/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility functions for RHEL Lightspeed Evaluation."""
-
-from rhel_lightspeed_evaluation.utils.config_loader import ExtendedConfigLoader
-
-__all__ = ["ExtendedConfigLoader"]
